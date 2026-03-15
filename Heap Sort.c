78ad809684d8c86df09ff36f840d0a1b54f604ee
Avijit Baral <avijitbaralwork@gmail.com>
2026-03-16 01:24:54 +0530

    Update Heap Sort.c

--- Heap Sort.c
+++ Heap Sort.c
@@ -1,37 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-void heapSort(int arr[], int n);
-void heapify(int arr[], int n, int i);
+void heapSort(int arr[],int n);
+void heapify(int arr[],int n,int i);
 void swap(int *a,int *b);
 
-int main(){
-
+int main()
+{
     FILE *file;
     int *arr;
-    int n=0;
+    int n=0,i;
+
+    arr=(int*)malloc(200000*sizeof(int));
 
     file=fopen("input_100000.txt","r");
 
-    arr=malloc(200000*sizeof(int));
+    if(file==NULL)
+    {
+        printf("File not found\n");
+        return 1;
+    }
 
     while(fscanf(file,"%d",&arr[n])==1)
         n++;
 
     fclose(file);
 
-    clock_t start=clock();
     heapSort(arr,n);
-    clock_t end=clock();
 
-    printf("Heap Sort Time: %f seconds\n",(double)(end-start)/CLOCKS_PER_SEC);
+    file=fopen("heap_sorted.txt","w");
+
+    for(i=0;i<n;i++)
+        fprintf(file,"%d\n",arr[i]);
+
+    fclose(file);
+
+    printf("Heap Sort completed\n");
 
     free(arr);
+    return 0;
 }
 
-void heapify(int arr[], int n, int i){
+void swap(int *a,int *b)
+{
+    int t=*a;
+    *a=*b;
+    *b=t;
+}
 
+void heapify(int arr[],int n,int i)
+{
     int largest=i;
     int l=2*i+1;
     int r=2*i+2;
@@ -39,25 +57,23 @@ void heapify(int arr[], int n, int i){
     if(l<n && arr[l]>arr[largest]) largest=l;
     if(r<n && arr[r]>arr[largest]) largest=r;
 
-    if(largest!=i){
+    if(largest!=i)
+    {
         swap(&arr[i],&arr[largest]);
         heapify(arr,n,largest);
     }
 }
 
-void heapSort(int arr[], int n){
+void heapSort(int arr[],int n)
+{
+    int i;
 
-    for(int i=n/2-1;i>=0;i--)
+    for(i=n/2-1;i>=0;i--)
         heapify(arr,n,i);
 
-    for(int i=n-1;i>0;i--){
+    for(i=n-1;i>0;i--)
+    {
         swap(&arr[0],&arr[i]);
         heapify(arr,i,0);
     }
-}
-
-void swap(int *a,int *b){
-    int t=*a;
-    *a=*b;
-    *b=t;
 }
