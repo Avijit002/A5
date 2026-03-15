ec2b8accbea6fcc401fa0002b0e7de4beeff6778
Avijit Baral <avijitbaralwork@gmail.com>
2026-03-16 01:23:34 +0530

    Update Merge Sort.c

--- Merge Sort.c
+++ Merge Sort.c
@@ -1,58 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-void mergeSort(int arr[], int l, int r);
-void merge(int arr[], int l, int m, int r);
+void mergeSort(int arr[],int l,int r);
+void merge(int arr[],int l,int m,int r);
 
-int main(){
-
+int main()
+{
     FILE *file;
     int *arr;
-    int n=0;
+    int n=0,i;
+
+    arr=(int*)malloc(200000*sizeof(int));
 
     file=fopen("input_100000.txt","r");
 
-    arr=malloc(200000*sizeof(int));
+    if(file==NULL)
+    {
+        printf("File not found\n");
+        return 1;
+    }
 
     while(fscanf(file,"%d",&arr[n])==1)
         n++;
 
     fclose(file);
 
-    clock_t start=clock();
     mergeSort(arr,0,n-1);
-    clock_t end=clock();
 
-    printf("Merge Sort Time: %f seconds\n",(double)(end-start)/CLOCKS_PER_SEC);
+    file=fopen("merge_sorted.txt","w");
+
+    for(i=0;i<n;i++)
+        fprintf(file,"%d\n",arr[i]);
+
+    fclose(file);
+
+    printf("Merge Sort completed\n");
 
     free(arr);
+    return 0;
 }
 
-void merge(int arr[], int l, int m, int r){
+void merge(int arr[],int l,int m,int r)
+{
+    int i,j,k;
+    int n1=m-l+1;
+    int n2=r-m;
 
-    int i=l,j=m+1,k=0;
-    int *temp=malloc((r-l+1)*sizeof(int));
+    int *L=(int*)malloc(n1*sizeof(int));
+    int *R=(int*)malloc(n2*sizeof(int));
 
-    while(i<=m && j<=r){
-        if(arr[i]<=arr[j]) temp[k++]=arr[i++];
-        else temp[k++]=arr[j++];
+    for(i=0;i<n1;i++)
+        L[i]=arr[l+i];
+
+    for(j=0;j<n2;j++)
+        R[j]=arr[m+1+j];
+
+    i=0;j=0;k=l;
+
+    while(i<n1 && j<n2)
+    {
+        if(L[i]<=R[j])
+            arr[k++]=L[i++];
+        else
+            arr[k++]=R[j++];
     }
 
-    while(i<=m) temp[k++]=arr[i++];
-    while(j<=r) temp[k++]=arr[j++];
+    while(i<n1) arr[k++]=L[i++];
+    while(j<n2) arr[k++]=R[j++];
 
-    for(i=l,k=0;i<=r;i++,k++)
-        arr[i]=temp[k];
-
-    free(temp);
+    free(L);
+    free(R);
 }
 
-void mergeSort(int arr[], int l, int r){
+void mergeSort(int arr[],int l,int r)
+{
+    int m;
 
-    if(l<r){
-
-        int m=(l+r)/2;
+    if(l<r)
+    {
+        m=l+(r-l)/2;
 
         mergeSort(arr,l,m);
         mergeSort(arr,m+1,r);
