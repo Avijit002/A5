5ee4bb6ff4976196d983c251d49336f0e79859e2
Avijit Baral <avijitbaralwork@gmail.com>
2026-03-16 01:22:57 +0530

    Update Insertion Sort.c

--- Insertion Sort.c
+++ Insertion Sort.c
@@ -1,41 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 void insertionSort(int arr[], int n);
 
-int main(){
-
+int main()
+{
     FILE *file;
     int *arr;
-    int n=0;
+    int n=0,i;
+
+    arr=(int*)malloc(200000*sizeof(int));
 
     file=fopen("input_100000.txt","r");
 
-    arr=malloc(200000*sizeof(int));
+    if(file==NULL)
+    {
+        printf("File not found\n");
+        return 1;
+    }
 
     while(fscanf(file,"%d",&arr[n])==1)
         n++;
 
     fclose(file);
 
-    clock_t start=clock();
     insertionSort(arr,n);
-    clock_t end=clock();
 
-    printf("Insertion Sort Time: %f seconds\n",(double)(end-start)/CLOCKS_PER_SEC);
+    file=fopen("insertion_sorted.txt","w");
+
+    for(i=0;i<n;i++)
+        fprintf(file,"%d\n",arr[i]);
+
+    fclose(file);
+
+    printf("Insertion Sort completed\n");
 
     free(arr);
+    return 0;
 }
 
-void insertionSort(int arr[], int n){
+void insertionSort(int arr[],int n)
+{
     int i,key,j;
 
-    for(i=1;i<n;i++){
+    for(i=1;i<n;i++)
+    {
         key=arr[i];
         j=i-1;
 
-        while(j>=0 && arr[j]>key){
+        while(j>=0 && arr[j]>key)
+        {
             arr[j+1]=arr[j];
             j--;
         }
