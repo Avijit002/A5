13e4b79fc592303917af6ddc31b18335684d7318
Avijit Baral <avijitbaralwork@gmail.com>
2026-03-16 01:23:15 +0530

    Update Selection Sort.c

--- Selection Sort.c
+++ Selection Sort.c
@@ -1,49 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 void selectionSort(int arr[], int n);
 void swap(int *a,int *b);
 
-int main(){
-
+int main()
+{
     FILE *file;
     int *arr;
-    int n=0;
+    int n=0,i;
+
+    arr=(int*)malloc(200000*sizeof(int));
 
     file=fopen("input_100000.txt","r");
 
-    arr=malloc(200000*sizeof(int));
+    if(file==NULL)
+    {
+        printf("File not found\n");
+        return 1;
+    }
 
     while(fscanf(file,"%d",&arr[n])==1)
         n++;
 
     fclose(file);
 
-    clock_t start=clock();
     selectionSort(arr,n);
-    clock_t end=clock();
 
-    printf("Selection Sort Time: %f seconds\n",(double)(end-start)/CLOCKS_PER_SEC);
+    file=fopen("selection_sorted.txt","w");
+
+    for(i=0;i<n;i++)
+        fprintf(file,"%d\n",arr[i]);
+
+    fclose(file);
+
+    printf("Selection Sort completed\n");
 
     free(arr);
+    return 0;
 }
 
-void selectionSort(int arr[], int n){
+void swap(int *a,int *b)
+{
+    int t=*a;
+    *a=*b;
+    *b=t;
+}
+
+void selectionSort(int arr[],int n)
+{
     int i,j,min;
 
-    for(i=0;i<n-1;i++){
+    for(i=0;i<n-1;i++)
+    {
         min=i;
+
         for(j=i+1;j<n;j++)
             if(arr[j]<arr[min])
                 min=j;
 
-        swap(&arr[i],&arr[min]);
+        swap(&arr[min],&arr[i]);
     }
-}
-
-void swap(int *a,int *b){
-    int t=*a;
-    *a=*b;
-    *b=t;
 }
