12c594cb8d20e1002c5d88d96ead9fb26894da35
Avijit Baral <avijitbaralwork@gmail.com>
2026-03-16 01:22:36 +0530

    Update Bubble Sort.c

--- Bubble Sort.c
+++ Bubble Sort.c
@@ -1,55 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 void bubbleSort(int arr[], int n);
 void swap(int *a, int *b);
-void writeToFile(int arr[], int n);
 
-int main() {
-
+int main()
+{
     FILE *file;
     int *arr;
-    int n = 0;
-    clock_t start,end;
+    int n = 0, i;
+
+    arr = (int*)malloc(200000 * sizeof(int));
 
     file = fopen("input_100000.txt","r");
 
-    arr = malloc(200000*sizeof(int));
+    if(file == NULL)
+    {
+        printf("File not found\n");
+        return 1;
+    }
 
-    while(fscanf(file,"%d",&arr[n])==1)
+    while(fscanf(file,"%d",&arr[n]) == 1)
         n++;
 
     fclose(file);
 
-    start = clock();
     bubbleSort(arr,n);
-    end = clock();
 
-    printf("Bubble Sort Time: %f seconds\n",(double)(end-start)/CLOCKS_PER_SEC);
+    file = fopen("bubble_sorted.txt","w");
 
-    writeToFile(arr,n);
+    for(i=0;i<n;i++)
+        fprintf(file,"%d\n",arr[i]);
+
+    fclose(file);
+
+    printf("Bubble Sort completed\n");
 
     free(arr);
+    return 0;
 }
 
-void bubbleSort(int arr[], int n){
-    int i,j;
-    for(i=0;i<n-1;i++)
-        for(j=0;j<n-i-1;j++)
-            if(arr[j]>arr[j+1])
-                swap(&arr[j],&arr[j+1]);
-}
-
-void swap(int *a,int *b){
+void swap(int *a,int *b)
+{
     int t=*a;
     *a=*b;
     *b=t;
 }
 
-void writeToFile(int arr[], int n){
-    FILE *f=fopen("bubble_sorted.txt","w");
-    for(int i=0;i<n;i++)
-        fprintf(f,"%d\n",arr[i]);
-    fclose(f);
+void bubbleSort(int arr[],int n)
+{
+    int i,j;
+
+    for(i=0;i<n-1;i++)
+        for(j=0;j<n-i-1;j++)
+            if(arr[j]>arr[j+1])
+                swap(&arr[j],&arr[j+1]);
 }
